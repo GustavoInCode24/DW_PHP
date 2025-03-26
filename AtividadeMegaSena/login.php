2364dc493d3e2dc15fe9f1db265a058138988a33
Gustavo Lemos  Oliveira <gugaprogramador24@gmail.com>
2025-03-25 23:58:11 -0300

    Add files via upload

---
+++ AtividadeMegaSena/login.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Casa de Aposta</title>
+</head>
+<body>
+
+    <div id="borda">
+    <div class="form-usuario">
+        <form action="#" method="POST">
+            <label for="usuario">Digite um email</label><br>
+            <input type="text" id="usuario" name="usuario" required><br>
+    
+            <label for="senha">Digite sua senha</label><br>
+            <input type="password" id="senha" name="senha" required><br><br>
+               
+            <input type="submit" value="Sair" name="btnSair">
+            <input type="submit" value="Entrar" name="btnUser">
+        </form>
+    </div>
+</div>
+
+
+        <?php 
+
+        if($_POST){
+
+            $email = "aluno@fatec.edu.br";
+            $senha = "alunoweb2";
+
+            $email = $_POST ['usuario'];
+            $senha = $_POST['senha'];
+
+        }
+            
+        ?>
+
+
+</body>
+</html>
